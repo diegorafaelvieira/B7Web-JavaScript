9a6453f9a9dbd7dd69a4af228960c31c9008d2ce
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2024-03-08 01:28:58 -0300

    Add Exercício 2

---
+++ Módulo 01 - Primeiros Passos/assets/js/exercicio1.js
@@ -0,0 +1,27 @@
+let nome = "Diego"; // string
+let idade = 39; // number
+let logado = true; // boolean
+let lista = ["ovo", "massa", "corante"]; //array
+let nomeCompleto = {nome: "Diego", idade: 39}; // object
+
+// o comando exibe os dados no Console
+console.log(typeof nomeCompleto);
+
+/*
+comentário 
+várias 
+linhas
+*/
+
+// 1. Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba-a.
+let carro = "Ferrari";
+console.log(carro);
+
+// 2. Crie uma variável para armazenar o preço do bolo, que custa R$19,99. E exiba-a. 
+let precoBolo = 19.99;
+console.log(precoBolo);
+
+// 3.Corrija o código a seguir e exiba-o.
+// let "cidade" =  São Paulo
+let cidade = "São Paulo";
+console.log(cidade); 
---
+++ Módulo 01 - Primeiros Passos/assets/js/templateString.js
@@ -0,0 +1,10 @@
+let nome = "Diego";
+let sobrenome = "Vieira";
+let idade = 39;
+
+//let nomeCompleto = nome + ' ' + sobrenome;
+let nomeCompleto = `${nome} ${sobrenome}`;
+let idadeString = `Idade: ${idade + 1} anos`;
+
+console.log("Nome: " + nomeCompleto);
+console.log(idadeString);

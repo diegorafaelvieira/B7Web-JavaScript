0564d8d499dc5b3bc9da5dba017b9c2057455f8c
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2024-03-10 23:42:09 -0300

    Add Aula Array

---
+++ Módulo 02 - JavaScript Básico/assets/js/arrays.js
@@ -0,0 +1,9 @@
+let nomes = ['bonieky', 'pedro', 'silvia'];
+
+let lista = ['blabla', nomes];
+
+let lista2 = ['blabla', ['xx', 'yy']];
+
+// Exibir o xx
+console.log(lista2[1][0]);
+
---
+++ Módulo 02 - JavaScript Básico/assets/js/arrowFunction.js
@@ -0,0 +1,31 @@
+/* function somar (x, y) {
+  return x + y;
+} */
+
+const somar = (x, y) => {
+  return x + y;
+};
+
+//const somar = (x + y) => x + y;
+
+console.log(somar(10, 5));
+
+/* function sobrenome(sob) {
+  return "Diego " + sob;
+} 
+
+const sobrenome = (sob) => {
+  let nomeCompleto = "Diego " + sob;
+  return nomeCompleto;
+};
+
+const sobrenome = (sob) => {
+  return "Diego " + sob;
+};
+
+const sobrenome = (sob) => "Diego " + sob;
+*/
+
+const sobrenome = (sob) => "Diego " + sob;
+
+console.log(sobrenome("Vieira"));

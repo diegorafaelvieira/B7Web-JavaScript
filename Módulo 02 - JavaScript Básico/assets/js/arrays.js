19c9841e9260739eb30ccaaa1018bd65e54c9771
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2024-03-11 00:15:16 -0300

    Add Exercício Array e organização exercícios

--- Módulo 02 - JavaScript Básico/assets/js/arrays.js
+++ Módulo 02 - JavaScript Básico/assets/js/arrays.js
@@ -7,3 +7,20 @@ let lista2 = ['blabla', ['xx', 'yy']];
 // Exibir o xx
 console.log(lista2[1][0]);
 
+let ingredientes = [
+  'agua',
+  'farinha',
+  'ovo',
+  'corante',
+  'sal'
+];
+
+ingredientes.push('cebola'); // Adiciona na última posição da array
+ingredientes.unshift('tomate'); // Adiciona na primeira posição do array 
+
+console.log(ingredientes);
+
+ingredientes.pop(); // Remove o último da array
+ingredientes.shift(); // Remove o primeiro da array
+
+console.log(`Total de ingredientes: ${ingredientes}`);
